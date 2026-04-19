f3e27750807767b678ad7b885895344a045c09f0
gusta <gusta@DESKTOP-M9L9GAN>
2026-04-19 15:04:45 -0300

    Add project files.

---
+++ devOpsPuc/Controllers/DevOpsPucController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace devOpsPuc.Controllers
+{
+    [ApiController]
+    [Route("DevOpsPuc")]
+
+    public class DevOpsPucController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
